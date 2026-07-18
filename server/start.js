b502ccc1b55d570576307364a7a9bf08466c9190
Madushan Bandara <madushansanjeewakg@gmail.com>
2026-07-18 22:46:05 +0200

    Load local environment overrides for development

--- server/start.js
+++ server/start.js
@@ -1,5 +1,7 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
 import app from './index.js';
+
+dotenv.config({ path: ['.env.local', '.env'] });
 
 const port = Number(process.env.PORT || 3000);
 app.listen(port, () => console.log(`Cargo API listening on http://localhost:${port}`));
